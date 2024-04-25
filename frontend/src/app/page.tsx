0a406231d006f9d6d546eb7143fef37e27e01066
Aatika hakim <aatikahakim90@gmail.com>
2024-04-25 11:23:36 +0500

    added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="w-full h-[100%]  pb-96 bg-gray-800 selection:text-white">
       <div className="flex justify-center items-center">
-        <h1 className="pt-32 pb-10 text-3xl md:text-5xl font-bold bg-gradient-to-r from-cyan-600 to-purple-700 inline-block text-transparent bg-clip-text">
+        <h1 className="pt-12 pb-10 text-3xl md:text-5xl font-bold bg-gradient-to-r from-cyan-600 to-purple-700 inline-block text-transparent bg-clip-text">
           Todo List
         </h1>
       </div>
